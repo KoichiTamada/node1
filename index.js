4938ac6e2e188ff27fb084462eb3398b0b5f6a72
k_tamachan <ktm.pen@gmail.com>
2020-07-10 20:40:03 +0900

    4th

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express=require('express');
+var ejs=require('ejs');
 var app=express();
 
+app.engine('ejs',ejs.renderFile);
+
 app.get('/',(req,res)=>{
-    res.send('Welcome to Express!');
+    res.render('index.ejs',
+    {title:'Index',
+        content:'This is Express-app Top Page!'
+    });
 });
 
 app.get('/other',(req,res)=>{
