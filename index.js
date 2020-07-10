72cdda68c602764efad20eba5a393c4d47aa50e4
k_tamachan <ktm.pen@gmail.com>
2020-07-10 20:11:17 +0900

    3rd

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/',(req,res)=>{
     res.send('Welcome to Express!');
 });
 
+app.get('/other',(req,res)=>{
+    res.send('Welcom other page!');
+})
+
 app.listen(3000,()=>{
     console.log('Start server port:3000');
 });
