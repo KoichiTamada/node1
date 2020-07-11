2df27e2ab70f95298ffeee5e7343e5e83b9dc344
k_tamachan <ktm.pen@gmail.com>
2020-07-11 18:54:05 +0900

    6

--- index.js
+++ index.js
@@ -6,15 +6,42 @@ app.engine('ejs',ejs.renderFile);
 app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
+    var msg='This is Express-app Top Page';
+    var url='/other?name=mon&pass=ktmpen8658';
     res.render('index.ejs',
-    {title:'Index',
-        content:'This is Express-app Top Page!'
+    {
+    title:'Index',
+    content:msg,
+    link1:{href:url,text:'other'},
+    link2:{href:'/hello',text:'hello'}
     });
 });
 
 app.get('/other',(req,res)=>{
-    res.send('Welcom other page!');
-})
+    var name = req.query.name;
+    var pass = req.query.pass;
+    var msg = 'This is Other Page!<br>'
+    + 'あなたの名前は「' + name +'」さんです。<br>'
+    + 'ﾊﾟｽﾜｰﾄﾞは「' + pass + '」です。';
+
+   res.render('other.ejs',
+   {
+    title:'other',
+    content:msg,
+    link1:{href:'/',text:'index'},
+    link2:{href:'/hello',text:'hello'}
+   });
+});
+
+app.get('/hello',(req,res)=>{
+    res.render('hello.ejs',
+    {
+    title:'Hello',
+    content:'This is Hello Page!',
+    link1:{href:'/',text:'index'},
+    link2:{href:'/other',text:'other'}
+    });
+});
 
 app.listen(3000,()=>{
     console.log('Start server port:3000');
